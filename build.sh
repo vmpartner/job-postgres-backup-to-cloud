eb4f58071efa451d99486d3ae6babc5f54847f23
vitams <vmpartner@gmail.com>
2021-09-27 19:37:20 +0500

    Up

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t vmpartner/job-postgres-backup-to-cloud:13-v1.1.0 .
-docker push vmpartner/job-postgres-backup-to-cloud:13-v1.1.0
+docker build -t vmpartner/job-postgres-backup-to-cloud:9-v1.1.0 .
+docker push vmpartner/job-postgres-backup-to-cloud:9-v1.1.0
