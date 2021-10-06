36718fbddd60c4de9e0ac4629c5c748736e45a07
vitams <vmpartner@gmail.com>
2021-10-06 13:28:47 +0500

    Up

--- run.sh
+++ run.sh
@@ -6,31 +6,31 @@ SRC=$(date '+%d%m%Y%H%M%S').sql.gz
 
 # BACKUP
 ##########################################################
-rm -f /dump.sql || true
+rm -f /backup/dump.sql || true
 echo "$POSTGRES_HOST:$POSTGRES_PORT:$POSTGRES_DATABASE:$POSTGRES_USER:$POSTGRES_PASSWORD" > /root/.pgpass
 chmod 0600 /root/.pgpass
-pg_dump --create --file=/dump.sql --format=c --dbname="$POSTGRES_DATABASE" --username="$POSTGRES_USER" --host="$POSTGRES_HOST" --port="$POSTGRES_PORT" -v
+pg_dump --create --file=/backup/dump.sql --format=c --dbname="$POSTGRES_DATABASE" --username="$POSTGRES_USER" --host="$POSTGRES_HOST" --port="$POSTGRES_PORT" -v
 
-# GZIP
+# STAT
 ##########################################################
-cd /
-rm -f dump.sql.gz
-gzip dump.sql
-mv dump.sql.gz "${SRC}"
+FILESIZE=$(stat -c%s /backup/${SRC})
+echo "Size of ${SRC} = $FILESIZE bytes."
 
 # COPY
 ##########################################################
-echo "Start $(date '+%d-%m-%Y %H:%M:%S')"
-FILESIZE=$(stat -c%s /${SRC})
-echo "Size of ${SRC} = $FILESIZE bytes."
-rclone copy /${SRC} "$RCLONE_DEST"
-rm -f "/${SRC}"
-echo "Finish $(date '+%d-%m-%Y %H:%M:%S')"
+if [ "${RCLONE_DEST}" = "**None**" ]; then
+  echo "INFO: Define RCLONE_DEST for upload backup to remote server"
+else
+  echo "Start $(date '+%d-%m-%Y %H:%M:%S')"
+  rclone copy /backup/${SRC} "$RCLONE_DEST"
+  rm -f /backup/${SRC}
+  echo "Finish $(date '+%d-%m-%Y %H:%M:%S')"
+fi
 
 # CHECK
 ##########################################################
 if [ "${CHECK_URL}" = "**None**" ]; then
-  echo "INFO: Define CHECK_URL with https://healthchecks.io to monitor sync job"
+  echo "INFO: Define CHECK_URL to monitor sync job"
 else
   echo "Curl check url: ${CHECK_URL}"
   curl "${CHECK_URL}"
@@ -39,7 +39,7 @@ fi
 # LOKI
 ##########################################################
 if [ "${LOKI_URL}" = "**None**" ]; then
-  echo "INFO: Define LOKI_URL to monitor sync job"
+  echo "INFO: Define LOKI_URL for send ping to Grafana Loki"
 else
   echo "Curl check url and app: ${LOKI_URL} ${LOKI_APP}"
   DT=$(date +%s%N)
